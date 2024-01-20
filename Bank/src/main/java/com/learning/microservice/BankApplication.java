6ed78ac325d1317fd314034cff2b97559f4fea34
BHEEMRAV-MAHOVIYA <bheemravma.ora@gmail.com>
2024-01-20 17:06:05 +0530

    Basic setup for zuul getway application

---
+++ Bank/src/main/java/com/learning/microservice/BankApplication.java
@@ -0,0 +1,15 @@
+package com.learning.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+}
